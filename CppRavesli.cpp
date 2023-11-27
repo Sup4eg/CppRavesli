20ae8f056e41446b37eeaf064d46d26d215be889
Kirill Trezubov <kirill3zoff@gmail.com>
2023-11-27 15:15:24 +0500

    CppRavesli-add: Finish Lesson60

--- CppRavesli.cpp
+++ CppRavesli.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
 
 
 class Lesson59 {
@@ -7,12 +9,36 @@ class Lesson59 {
 public:
 
   static float cStyleConversion(int i1, int i2) {
+    //best not to use, doesn't check in compile time
     return (float) i1 / i2;
   }
 
   static float staticCastConversion(int i1, int i2) {
+    //best to use, hase checking in compile 
     return static_cast<float>(i1) / i2;
   }
+};
+
+class Lesson60 {
+  //Strings
+public:
+  
+  static void pickAndEnterName() {
+    cout << "Pick 1 or 2: ";
+    int choice;
+    cin >> choice;
+
+    //ignore '\n' here
+    cin.ignore(32767, '\n');
+
+    cout << "Now enter your name: ";
+    string myName;
+    getline(cin, myName);
+
+    cout << "Hello, " << myName << ", you picked " << choice << '\n';
+
+  }
+
 };
 
 
@@ -22,4 +48,5 @@ public:
 int main() {
   cout << Lesson59::cStyleConversion(11, 3) << endl;
   cout << Lesson59::staticCastConversion(11, 3) << endl;
+  Lesson60::pickAndEnterName();
 }
